3666b3658bef87191d601dfdd74d242dbc5b40f0
emmekd <mimikooppler12@gmail.com>
2026-05-14 19:55:42 -0600

    Si

---
+++ nodo.py
@@ -0,0 +1,7 @@
+# nodo.py
+class Nodo:
+    def __init__(self, valor):
+        self.valor = valor
+        self.izquierdo = None
+        self.derecho = None
+        self.altura = 1  # útil para AVL
